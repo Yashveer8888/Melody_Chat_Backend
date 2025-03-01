bab8116efe1c640f2f950ceff1ac6ca9751edca9
Yashveer Singh <124042721+Yashveer8888@users.noreply.github.com>
2025-03-01 23:40:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5000",
     methods: ["GET", "POST", "DELETE"],
   },
 });
@@ -159,7 +159,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
