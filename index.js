625b002cbc3c0593ecd6744a3b3d36d94f62ea14
Yashveer Singh <124042721+Yashveer8888@users.noreply.github.com>
2025-03-01 23:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5000",
+    origin: "https://melody-chat-backend.onrender.com",
     methods: ["GET", "POST", "DELETE"],
   },
 });
